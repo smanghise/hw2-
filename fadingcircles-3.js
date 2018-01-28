a4e5745531ba0fa8b0b0b42cbf9e40a0b51e483f
smanghise <35621921+smanghise@users.noreply.github.com>
2018-01-27 22:05:35 -0800

    Update fadingcircles-3.js

--- fadingcircles-3.js
+++ fadingcircles-3.js
@@ -5,5 +5,5 @@ noStroke();
 while(true) {
   background(0, 0, 255, 0.1);
   fill(random(210,270), 255, 255);
-  ellipse(random(width), random(height), random(20, 40));
+  ellipse(random(width), random(height), random(30,80));
 }
