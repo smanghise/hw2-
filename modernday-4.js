cc40de404abb1dbbfcce315e86d84b3afcb62e81
smanghise <35621921+smanghise@users.noreply.github.com>
2018-01-29 09:39:48 -0800

    Update modernday-4.js

--- modernday-4.js
+++ modernday-4.js
@@ -1,1 +1,36 @@
+function setup() {
+  createCanvas(340, 340);
+}
+
+/*
+function draw() {
+	strokeWeight(4);
+  fill('blue');
+  rect(random(width), random(height), random(width), random(height));
+  strokeWeight(4);
+  fill('red');
+  rect(random(width), random(height), random(width), random(height));
+  strokeWeight(4);
+  fill('white');
+  rect(random(width), random(height), random(width), random(height));
+  strokeWeight(4);
+  fill('yellow');
+  rect(random(width), random(height), random(width), random(height));
+  strokeWeight(4);
+  fill('black');
+  rect(random(width), random(height), random(width), random(height));
+}
+*/
+
+
+var mondrian_colors = ['blue','red','white','black','yellow']
+           
+
+function draw(color) {
+  for (var color in mondrian_colors) {
+    strokeWeight(4);
+    fill(random(color));
+    rect(random(width), random(height), random(width), random(height));
+	}
+}
 
